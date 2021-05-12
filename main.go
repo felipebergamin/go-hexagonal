26c942e41cdec5688448c7d8df9bcd99fd6b7631
Felipe Bergamin <felipebergamin6@gmail.com>
2021-05-11 22:53:57 -0300

    feat(cli): add cobra commands

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
+/*
+Copyright © 2021 Felipe Ricieri Bergamin <felipebergamin6@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/felipebergamin/go-hexagonal/adapters/db"
-	"github.com/felipebergamin/go-hexagonal/application"
-)
+import "github.com/felipebergamin/go-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-
-	product, _ := productService.Create("Example Product", 30)
-	productService.Enable(product)
+	cmd.Execute()
 }
